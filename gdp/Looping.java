31bf98c85d5244c54207d11def8166531e730365
valentin <valebr@gmx.de>
2023-12-01 23:07:51 +0100

    2

---
+++ gdp/Looping.java
@@ -0,0 +1,23 @@
+public class Looping {
+    long N = 106;//Long.parseLong(args[0]);
+    long m
+    public static void main(String[] args) {
+        if (Long.parseLong[].length >=0 ) 
+            {System.out.println("");
+            System.exit(-1);
+        }
+            long N1=N; // Originalwert von N speichern
+            long N2=N;
+            long N3=N;
+            
+    static long reverse () {
+        while (N != 0) {
+            long lastDigit = N % 10; // get last digit of N
+            R = R * 10 + lastDigit; // Add the last digit to R and digit shift the previous value of R
+            N = (int) N / 10; // remove the last digit from N
+    
+        }
+    }
+}
+    
+
