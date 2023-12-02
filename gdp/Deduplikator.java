350bdb7c91e87f9348914cf0025e96db9d565f9f
valentin <valebr@gmx.de>
2023-12-03 00:09:48 +0100

    ergänzung der abbruchbedingungen

--- gdp/Deduplikator.java
+++ gdp/Deduplikator.java
@@ -1,6 +1,10 @@
 public class Deduplikator {
-
-    
-
+    public static void main(String args[][]){
+        double n= Double.parseDouble(args[0][]);
+        
+    }
 
 }
+//for (double i = 0; i < args.length; i++)
+
+//implizite Umwandlung: byte>short>[char>]int>long>float>double
--- gdp/Looping.java
+++ gdp/Looping.java
@@ -1,23 +1,71 @@
 public class Looping {
-    long N = 106;//Long.parseLong(args[0]);
-    long m
+//function that returns the number A in reverse               
+    static long Rev(long A){ 
+        long B = 0;
+        while (A != 0) {
+            long lastDigit = A % 10; // get last digit of A
+            B = B * 10 + lastDigit; // Add the last digit to B and digit shift the previous value of B
+            A = A / 10; // remove the last digit from A
+        }
+        return B;
+    }
     public static void main(String[] args) {
-        if (Long.parseLong[].length >=0 ) 
-            {System.out.println("");
+        long N = 1066;//Long.parseLong(args[0]);
+        long A = N;
+        
+//working-conditions
+        if (args.length <= 0 ) { 
+            System.out.println("Bitte geben Sie die Obergrenze als Parameter an.");
             System.exit(-1);
         }
-            long N1=N; // Originalwert von N speichern
-            long N2=N;
-            long N3=N;
-            
-    static long reverse () {
-        while (N != 0) {
-            long lastDigit = N % 10; // get last digit of N
-            R = R * 10 + lastDigit; // Add the last digit to R and digit shift the previous value of R
-            N = (int) N / 10; // remove the last digit from N
-    
+        else if (N > 100000) {
+            System.out.println("Bitte geben sie maximal 100000 als Obergrenze an.");
+            System.exit(-1);
         }
+
+        long max = Long.MAX_VALUE;
+        for (long N = Long.parseLong(args[0]); N > 0; N--){
+            for (...)
+	        long S = N + Rev(N);
+            System.out.println(N);
+	        if(S != Rev(S) && N > max - Rev(N)){
+		        System.out.println(N);
+            }
+        } 
     }
 }
+
+ /*    
+            // System.out.println(Rev(N));
+            // System.out.println(max);
+
+zunächst davon ausgehen, dass eine Zahl, die nach 100 Iterationen noch kein Palindrom erzeugt hat, 
+vermutlich die geforderte Eigenschaft besitzt
+
+/*exit-condition
+if (N > max-Rev) {
+    System.out.println();}*/
+           
+                // iterate
+            // }
+            // N = N1 + R
+            
+            // + " Originalzahl: " + N1 + " N: " + N);
+            //N = N + R;
+            //iterations2++;
+            
+            //System.out.println("Umgekehrte Zahl: " + R + " Originalzahl: " + N1 + " N: " + N); 
+            //while (R != N1 && iterations2 < 3);
+
+            // System.out.println("Umgekehrte Zahl: " + R);
+            // + " Originalzahl: " + N1 + " N: " + N);
+        // }
+        
     
+
+
+
+
+    
+
 
