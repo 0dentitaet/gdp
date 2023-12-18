5ae26e6b43d91872ed99c2cf75b443b52932b1ae
0dentitaet <152170257+0dentitaet@users.noreply.github.com>
2023-12-18 19:39:38 +0100

    Update PlaetzchenPruefer.java

--- gdp/PlaetzchenPruefer.java
+++ gdp/PlaetzchenPruefer.java
@@ -2,6 +2,7 @@ public class PlaetzchenPruefer {
     public static void main(String[] args) {
             
     }
+    
 
     public static int pruefePlaetzchen(int anzahlPlaetzchen) {
         int n = 0;
