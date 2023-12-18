175418f52c04b89a233cc65c66abb3dc4bb036f8
valentin <valebr@gmx.de>
2023-12-18 19:36:55 +0100

    8, 15

--- gdp/KochSchneeflocke.java
+++ gdp/KochSchneeflocke.java
@@ -1,4 +1,7 @@
 class KochSchneeflocke {
     
+    public static void zeichneKochKurve(int n, double x1, double y1, double x5, double y5){
 
+    }
+    
 }
---
+++ gdp/Riddle.java
@@ -0,0 +1,15 @@
+class Riddle {
+
+
+
+
+}
+
+
+
+
+
+/*Q: Gibt es ein Zeitlimit für die Ausführung der main-Funktion?
+    A: Ja, für N≤12 sollte das Resultat in unter 2 Sekunden vorliegen. Über 12 darf es (u.U. sehr viel) länger dauern.
+Q: Sind externe Abhängigkeiten (z.B. Set oder HashSet) erlaubt?
+    A: Nein, externe Abhängigkeiten sind nicht erlaubt. */
