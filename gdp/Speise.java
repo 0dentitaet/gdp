05a03ae34b873bdea9187942808a3a906fa43812
valentin <valebr@gmx.de>
2024-01-21 16:11:40 +0100

    minor changes

--- gdp/Speise.java
+++ gdp/Speise.java
@@ -2,13 +2,13 @@ public class Speise extends Lebensmittel {
 
 //Lebensmittel.essen()
 
-    protected int essen(int menge){
+    if protected int essen(int menge){
         this.menge -= menge;
         return this.menge;
     }
     
-    protected boolean trinken(int menge){
-        return false;
+        protected boolean trinken(int menge){
+    else    return false;
     }
 
 
